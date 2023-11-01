35c42e2609c7f6793c44c0b222ba68fc4bb16941
Ali <habibullaecm69@gmail.com>
2023-11-02 01:13:11 +0600

    news

--- src/Companents/Layout/Header/Header.jsx
+++ src/Companents/Layout/Header/Header.jsx
@@ -1,38 +1,69 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import s from './Header.module.css';
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="container">
-                <div className="header__logo-telegram">
-                    <div className="header__logo">
-                        <img src="1.jpg" alt="Логотип"/>
-                    </div>
-                    <div className="header__centeredText">
-                        Официальный Портал Государственных Закупок Кыргызской Республики
-                    </div>
-                    <div className="header__telegram">
-                        <a href="https://web.telegram.org/a/" target="_blank" rel="noopener noreferrer">
-                            <img src="tg.svg" alt="Telegram"/>
-                        </a>
-                    </div>
-                </div>
-                <hr/>
-                <nav>
-                    <div className="header__navbar">
-                        <Link className="header__links" to="#">Закупки</Link>
-                        <Link className="header__links" to="#">Контракты</Link>
-                        <Link className="header__links" to="/catalog">Каталог</Link>
-                        <Link className="header__links" to="#">Планирование</Link>
-                        <Link className="header__links" to="#">Объявление</Link>
-                        <Link className="header__links" to="#">Аналитические данные</Link>
-                        <Link className="header__links" to="#">Планы</Link>
-                    </div>
-                </nav>
-            </div>
-        </header>
-    );
-};
+  return (
+    <header className={s.header}>
+      {/* First Block: Search and Region Selection */}
+      <div className={s['search-region']}>
+        <div className={s.search}>
+          <input type="text" placeholder="Поиск..." />
+        </div>
+        <div className={s['region-select']}>
+          <select>
+            <option value="Бишкек">Бишкек</option>
+            <option value="Ош">Ош</option>
+            <option value="Баткен">Баткен</option>
+            <option value="Ыссык Куль">Ыссык Куль</option>
+            <option value="Нарын">Нарын</option>
+            <option value="Талас">Талас</option>
+            <option value="Жалал-Абад">Жалал-Абад</option>
+          </select>
+        </div>
+      </div>
 
+      {/* Second Block: Logo and Telegram Link */}
+      <div className={s['logo-telegram']}>
+        <div className={s.logo}>
+          <img src="1.jpg" alt="Логотип" />
+        </div>
+        <div className={s.centeredText}>
+          Официальный Портал Государственных Закупок Кыргызской Республики
+        </div>
+        <div className={s.telegram}>
+          <a href="https://web.telegram.org/a/" target="_blank" rel="noopener noreferrer">
+            <img src="tg.svg" alt="Telegram"/> 
+          </a>
+        </div>
+      </div>
+
+      {/* Third Block: Navigation Panel */}
+      <nav>
+        <ul className={s['navbar']}>
+          <li><a href="#">Закупки</a></li>
+          <li><a href="#">Контракты</a></li>
+          <li><a href="#">Каталог</a></li>
+          <li><a href="#">Планирование</a></li>
+          <li><a href="#">Объявление</a></li>
+          <li><a href="#">Аналитические данные</a></li>
+          <li><a href="#">Планы</a></li>
+        </ul>
+        <div className={s['menu-select']}>
+          <select>
+            <option value="Бишкек">Закупки</option>
+            <option value="Ош">Контракты</option>
+            <option value="Баткен">Каталог</option>
+            <option value="Ыссык Куль">Планирование</option>
+            <option value="Нарын">Объявление</option>
+            <option value="Талас">Аналитические данные</option>
+            <option value="Жалал-Абад">Планы</option>
+          </select>
+        </div>
+      </nav>
+    </header>
+  );
+};
+``
 export default Header;
+
+
--- src/pages/news/news.jsx
+++ src/pages/news/news.jsx
@@ -1,11 +1,79 @@
 import React from 'react';
+import style from './News.module.css';
 
 const News = () => {
-    return (
-        <section className="news">
-            <h1>news</h1>
-        </section>
-    );
+  const newsData = [
+    {
+      date: '2023-10-28',
+      title: 'Заголовок новости 1',
+      content: 'Содержание новости 1...',
+    },
+    {
+      date: '2023-10-27',
+      title: 'Заголовок новости 2',
+      content: 'Содержание новости 2...',
+    },
+    {
+      date: '2023-10-28',
+      title: 'Заголовок новости 1',
+      content: 'Содержание новости 1...',
+    },
+    {
+      date: '2023-10-27',
+      title: 'Заголовок новости 2',
+      content: 'Содержание новости 2...',
+    },
+    {
+      date: '2023-10-28',
+      title: 'Заголовок новости 1',
+      content: 'Содержание новости 1...',
+    },
+    {
+      date: '2023-10-27',
+      title: 'Заголовок новости 2',
+      content: 'Содержание новости 2...',
+    },
+  ];
+
+  const specialNewsData = [
+    'Каталог новойстей всех',
+    'Специальная новость 1',
+    'Специальная новость 1',
+    'Специальная новость 1',
+    'Специальная новость 1',
+    'Специальная новость 1',
+    'Специальная новость 1',
+    'Специальная новость 1',
+  ];
+
+  return (
+    <section className={style.news}>
+      <div className={style['news-container']}>
+        <div className={style['news-blocks']}>
+          {newsData.map((news, index) => (
+            <div key={index} className={style['news-block']}>
+              <div className={style['news-header']}>
+                <a href="#" className={style['news-title']}>{news.title}</a>
+                <div className={style['news-date']}>{news.date}</div>
+              </div>
+              <div className={style['news-content']}>{news.content}</div>
+            </div>
+          ))}
+        </div>
+        <div className={style['special-news']}>
+          <ul>
+            {specialNewsData.map((news, index) => (
+              <li key={index}>
+                <a href="#" className={style['special-news-link']}>{news}</a>
+              </li>
+              
+            ))}
+          </ul>
+
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default News;
