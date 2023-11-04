e5f1171f55afa7d55801072a3ec97dcc7145eb0b
Beka <kydyrmaevbekbolot6@gmail.com>
2023-11-04 13:05:05 +0600

    fix footer zakupki

--- src/Companents/Layout/Footer/Footer.jsx
+++ src/Companents/Layout/Footer/Footer.jsx
@@ -9,15 +9,7 @@ const Footer = () => {
     return (
         <footer className={style.footer}>
             <div className={style.container}>
-                <nav>
-                    <ul className={style['nav-list']}>
-                        <li><a href="/supplier">Поставщикам</a></li>
-                        <li><a href="/customer">Заказчикам</a></li>
-                        <li><a href="/statistics">Статистика</a></li>
-                        <li><a href="/sitemap">Карта сайта</a></li>
-                    </ul>
-                </nav>
-                <div className={style.divider}></div>
+                
                 <div className={style['logo-section']}>
                     <a href="">
                         <img src="../vite.svg" alt="LOGO" />
@@ -28,17 +20,39 @@ const Footer = () => {
                     <h1>Официальные ресурсы</h1>
                     <p>Здесь может быть ваш рыбный текст <br /> и ссылки на официальные источники.</p>
                 </div>
-                <div className={style['faq-section']}>
-                    <h1>Часто задаваемые <br /> вопросы и опрос</h1>
-                    <div className="telegram-section">
-                        <a href="https://t.me/your-telegram-channel" target="_blank" rel="noopener noreferrer">
-                            <img src="../telegram_icon.png" alt="Telegram" />
+                
+
+                <div className={style['contact-section']}>
+                    <h1>Контакты</h1>
+                    <div className="contact-section_left">
+                        <a href="tel=+999 (999) 99 99 99" >
+        
+                           <p>+996 (999) 99 99 99</p>
+        
+                           <p>+996 (999) 99 99 99</p>
+      
+                           <p>+996 (999) 99 99 99</p>
+        
+                           
                         </a>
                     </div>
                 </div>
-                <div className={style['back-to-top']}>
-                    <button onClick={scrollToTop}>Наверх</button>
-                </div>
+
+                {/* <div className={style['social-section']}>
+                    
+                <div className="telegram-section">
+                                    <a href="https://t.me/your-telegram-channel" target="_blank" rel="noopener noreferrer" className='telegram'>
+                                        <img src="../public/tg.svg" alt="Telegram" />
+                                        
+                                    </a>
+                                    <br />
+
+                                    <a href="https://t.me/your-telegram-channel" className='whats' target="_blank" rel="noopener noreferrer">
+                                        <img src="../public/whatsapp.svg" alt="Whatsapp" />
+                                    </a>
+                    </div>
+                </div> */}
+                
             </div>
         </footer>
     );
--- src/pages/ContractCards/ContractCard.jsx
+++ src/pages/ContractCards/ContractCard.jsx
@@ -1,74 +1,557 @@
 import React from 'react';
 import './ContractCards.scss'
+import {Link} from "react-router-dom";
+import './ContractCards'
 
 const ContractsCard = () => {
     return (
-        <div>
+        <div className='ContractsCard'>
             <div className="title-container">
                 <button onClick={() => window.history.back()}>Назад</button>
           <h2>Информация об организации</h2>
             </div>
-          
-          <div className="info-container">
-            <div className="info-section">
-              <h3>Наименование организации</h3>
-              <p>МУНИЦИПАЛЬНОЕ БЮДЖЕТНОЕ ДОШКОЛЬНОЕ ОБРАЗОВАТЕЛЬНОЕ УЧРЕЖДЕНИЕ <br /> "ЦЕНТР РАЗВИТИЯ РЕБЁНКА-ДЕТСКИЙ САД № 73"</p>
-            </div>
-            <hr className="divider" />
-            <div className="info-section">
-              <h3>Форма собственности</h3>
-              <p>МУНИЦИПАЛЬНОЕ БЮДЖЕТНОЕ ДОШКОЛЬНОЕ ОБРАЗОВАТЕЛЬНОЕ УЧРЕЖДЕНИЕ</p>
-            </div>
-            <hr className="divider" />
-            <div className="info-section">
-              <h3>ИНН организации</h3>
-              <p>02903201210019</p>
-            </div><hr className="divider" />
-            <div className="info-section">
-              <h3>Населённый пункт</h3>
-              <p>Сулюкта</p>
-            </div><hr className="divider" />
-            <div className="info-section">
-              <h3>Фактический адрес</h3>
-              <p>ул. Раззакова., д. 87</p>
-            </div><hr className="divider" />
-            <div className="info-section">
-              <h3>Рабочий телефон</h3>
-              <p>0365350074</p>
-            </div><hr className="divider" />
-            <div className="info-section">
-              <h3>Банк</h3>
-              <p>Центральное Казначейство</p>
-            </div><hr className="divider" />
-            <div className="info-section">
-              <h3>Р/счет</h3>
-              <p>4409051223000893</p>
-            </div><hr className="divider" />
-             <div className="info-section">
-              <h3>Валюта контракта</h3>
-              <p>Кыргызский сом (KGS)</p>
-            </div>
-            
-            <hr className="divider" />
-            <div className="info-section">
-              <h3>Дата заключения контракта</h3>
-              <p>22.09.2023</p>
-            </div>
-            <hr className="divider" />
-           <div className="info-section">
-              <h3>ФИО пользователя</h3>
-              <p>Жумагулов Талантбек Абдыкулович</p>
-              </div>
-            <hr className="divider" />
-            <div className="info-section">
-              <h3>Должность</h3>
-              <p>	Руководитель</p>
-            </div>
-            
-          </div>
+        <div className="infor-container">
+      <div className="top">
+      Общие сведения
+      </div>
+      <div className="contractCards-container">
+             
+                
+               
+                <div className="left-section">
+                    <div>
+                        <h3>Наименование закупки</h3>
+                        <p className="contractcards-maindetails">Поставка лекарственных препаратов для медицинского применения Инсулины</p>
+                    </div>
+                    
+                    
+                   <div className="bottom">
+                    <div className="left">
+                    Номер редакции извещения
+                    </div>
+                    <div className="right">
+                    1
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Идентификационный код закупки
+                    </div>
+                    <div className="right">
+                    232352801407835280
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Объект закупки
+                    </div>
+                    <div className="right">
+                    Поставка лекарственных препаратов для медицинского применения Инсулины
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Номер закупки
+                    </div>
+                    <div className="right">
+                    0330300002623000104
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Способ закупки
+                    </div>
+                    <div className="right">
+                    Запрос котировок в электронной форме
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Текущая стадия
+                    </div>
+                    <div className="right">
+                        <p className="contract-details, aplication" > Приём заявок </p>
+                    </div>
+                    
+                   </div>
+                   
+
+                    </div>
+                 
+               
+           
+                <div className="right-section">
+                    <h3>Начальная цена</h3>
+                    <p className="contractcards-price">188 888,25 KGS</p>
+
+                    <h3>Дата и время начала подачи заявок</h3>
+                    <p className="contract-price__under">27.10.2023
+                    19:08 | GMT+6</p>
+                    
+                    <h3>Дата и время начала подачи заявок</h3>
+                    <p className="contract-price__under">06.11.2023
+                    12:00 | GMT+6</p>
+                    
+                    <h3>Дата и время окончания подачи заявок</h3>
+                    <p className="contract-price__under">08.11.2023
+                    02:59| GMT+6</p>
+
+                    
+
+
+                </div>
+             </div>
+      
         </div>
+
+        <div className="infor-container">
+      <div className="top">
+      Сведения об организаторе
+      </div>
+      
+             
+                
+               
+                <div className="about_organization">
+  
+                   <div className="bottom">
+                    <div className="left">
+                    Наименование организации
+                    </div>
+                    <div className="right">
+                    БЮДЖЕТНОЕ УЧРЕЖДЕНИЕ ЗДРАВООХРАНЕНИЯ ВОЛОГОДСКОЙ ОБЛАСТИ "ВОЛОГОДСКАЯ ОБЛАСТНАЯ ДЕТСКАЯ БОЛЬНИЦА № 2"
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Тип организации
+
+                    </div>
+                    <div className="right">
+                    Заказчик
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    ИНН
+                    </div>
+                    <div className="right">
+                    3528014078
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    КПП
+                    </div>
+                    <div className="right">
+                    352801001
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Место
+                    </div>
+                    <div className="right">
+                    720084, Чуйская обл, Бишкек г, УЛ. КАДЖИСАЙ, Д.35
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Почтовый адрес
+                    </div>
+                    <div className="right">
+                    72039594, УЛ. КАДЖИСАЙ, Бишкек г, Чуйская обл,  Д.35
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Контактный телефон
+                    </div>
+                    <div className="right">
+                    +(999) 99-77-99
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Адрес электронной почты
+                    </div>
+                    <div className="right">
+                    bigtech@mail.ru
+                    </div>
+                    
+                   </div>
+
+                   <div className="bottom">
+                    <div className="left">
+                    Ответственное должностное лицо
+                    </div>
+                    <div className="right">
+                    Мазурак Арина Андреевна
+                    </div>
+                    
+                   </div>
+                   
+
+                    </div>
+                 
+               
+           
+
+             </div>
+      
+       
+
+<div className="wrapper">
+          <section className="details-group">
+            <details className="details" open>
+              <summary className="details__summary">
+                сведения о предмете закупки 
+              </summary>
+              <div className="details__content">
+              <div className="about_organization">
+  
+  <div className="bottom">
+   <div className="left">
+   Наименование организации
+   </div>
+   <div className="right">
+   БЮДЖЕТНОЕ УЧРЕЖДЕНИЕ ЗДРАВООХРАНЕНИЯ ВОЛОГОДСКОЙ ОБЛАСТИ "ВОЛОГОДСКАЯ ОБЛАСТНАЯ ДЕТСКАЯ БОЛЬНИЦА № 2"
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Тип организации
+
+   </div>
+   <div className="right">
+   Заказчик
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   ИНН
+   </div>
+   <div className="right">
+   3528014078
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   КПП
+   </div>
+   <div className="right">
+   352801001
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Место
+   </div>
+   <div className="right">
+   720084, Чуйская обл, Бишкек г, УЛ. КАДЖИСАЙ, Д.35
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Почтовый адрес
+   </div>
+   <div className="right">
+   72039594, УЛ. КАДЖИСАЙ, Бишкек г, Чуйская обл,  Д.35
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Контактный телефон
+   </div>
+   <div className="right">
+   +(999) 99-77-99
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Адрес электронной почты
+   </div>
+   <div className="right">
+   bigtech@mail.ru
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Ответственное должностное лицо
+   </div>
+   <div className="right">
+   Мазурак Арина Андреевна
+   </div>
+   
+  </div>
+  
+
+   </div>
+              </div>
+            </details>
+            <details className="details">
+              <summary className="details__summary">Ограничения, преимущества, требования к участникам и документам для предоставления</summary>
+              <div className="details__content">
+              <div className="about_organization">
+  
+  <div className="bottom">
+   <div className="left">
+   Наименование организации
+   </div>
+   <div className="right">
+   БЮДЖЕТНОЕ УЧРЕЖДЕНИЕ ЗДРАВООХРАНЕНИЯ ВОЛОГОДСКОЙ ОБЛАСТИ "ВОЛОГОДСКАЯ ОБЛАСТНАЯ ДЕТСКАЯ БОЛЬНИЦА № 2"
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Тип организации
+
+   </div>
+   <div className="right">
+   Заказчик
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   ИНН
+   </div>
+   <div className="right">
+   3528014078
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   КПП
+   </div>
+   <div className="right">
+   352801001
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Место
+   </div>
+   <div className="right">
+   720084, Чуйская обл, Бишкек г, УЛ. КАДЖИСАЙ, Д.35
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Почтовый адрес
+   </div>
+   <div className="right">
+   72039594, УЛ. КАДЖИСАЙ, Бишкек г, Чуйская обл,  Д.35
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Контактный телефон
+   </div>
+   <div className="right">
+   +(999) 99-77-99
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Адрес электронной почты
+   </div>
+   <div className="right">
+   bigtech@mail.ru
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Ответственное должностное лицо
+   </div>
+   <div className="right">
+   Мазурак Арина Андреевна
+   </div>
+   
+  </div>
+  
+
+   </div>
+              </div>
+            </details>
+            <details className="details">
+              <summary className="details__summary">Требование заказчика МИНИСТЕРСТВО ВНУТРЕННИХ ДЕЛ ПО РЕСПУБЛИКЕ ХАКАСИЯ</summary>
+              <div className="details__content">
+              <div className="about_organization">
+  
+  <div className="bottom">
+   <div className="left">
+   Наименование организации
+   </div>
+   <div className="right">
+   БЮДЖЕТНОЕ УЧРЕЖДЕНИЕ ЗДРАВООХРАНЕНИЯ ВОЛОГОДСКОЙ ОБЛАСТИ "ВОЛОГОДСКАЯ ОБЛАСТНАЯ ДЕТСКАЯ БОЛЬНИЦА № 2"
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Тип организации
+
+   </div>
+   <div className="right">
+   Заказчик
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   ИНН
+   </div>
+   <div className="right">
+   3528014078
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   КПП
+   </div>
+   <div className="right">
+   352801001
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Место
+   </div>
+   <div className="right">
+   720084, Чуйская обл, Бишкек г, УЛ. КАДЖИСАЙ, Д.35
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Почтовый адрес
+   </div>
+   <div className="right">
+   72039594, УЛ. КАДЖИСАЙ, Бишкек г, Чуйская обл,  Д.35
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Контактный телефон
+   </div>
+   <div className="right">
+   +(999) 99-77-99
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Адрес электронной почты
+   </div>
+   <div className="right">
+   bigtech@mail.ru
+   </div>
+   
+  </div>
+
+  <div className="bottom">
+   <div className="left">
+   Ответственное должностное лицо
+   </div>
+   <div className="right">
+   Мазурак Арина Андреевна
+   </div>
+   
+  </div>
+  
+
+   </div>
+              </div>
+            </details>
+            </section>
+</div>
+     
+ <div className="submit-button">
+        <button>Подать заявку</button>
+      </div>
+      
+
+
+
+        
+        
+</div>
+
+
       );
     };
 
+
+
 export default ContractsCard;
+
+
+
+
 
